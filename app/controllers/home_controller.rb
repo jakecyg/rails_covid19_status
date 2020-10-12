18997f1c20cdb48b09dd6d194534380c101f5526
jakecyg <43617272+jakecyg@users.noreply.github.com>
2020-10-12 00:18:02 -0500

    added Regions and Countries controller and edited sitenav

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     @regions = Region.order('name asc')
+    @wwConfirmed = PrimaryMetric.sum(:confirmed)
+    @wwDeaths = PrimaryMetric.sum(:deaths)
+    @wwRecovered = PrimaryMetric.sum(:recovered)
+    @wwActive = PrimaryMetric.sum(:active)
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'countries/index'
+  get 'countries/show'
+  get 'regions/index'
+  get 'regions/show'
+  get 'regiones/index'
+  get 'regiones/show'
   get 'about/index'
   get 'home/index'
   root to: "home#index"
